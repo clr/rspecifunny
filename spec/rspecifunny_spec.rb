ade3c61c7c2abc5c854fc0245857e51f444c319f
clr <clr@port49.com>
2009-03-09 16:35:04 -0400

    -- initial commit --

--- spec/rspecifunny_spec.rb
+++ spec/rspecifunny_spec.rb
@@ -1,7 +1,9 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe "Rspecifunny" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe "Pass for 1.8" do
+  it "should not keep separate local scope for lambda" do
+    a = lambda{ |b, c| a = b + c }
+    a.call( 2, 3 ).should equal( 5 )
+    a.should be_an_instance_of( Fixnum )
   end
 end
